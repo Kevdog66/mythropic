20e34a611c65d937dd4c9eae27e0ca89edf46667
Aditya Sharma <adi@Adityas-MacBook-Pro.local>
2025-05-30 16:09:11 -0400

    Add blog

---
+++ blog-page/data/headerNavLinks.ts
@@ -0,0 +1,9 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/tags', title: '' },
+  { href: '/projects', title: 'Projects' },
+  { href: '/About', title: 'About' },
+]
+
+export default headerNavLinks
